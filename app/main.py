21da7bbfa85be7164ab6d4da007718e4ef62cd6e
paolaqv <paola.quispe.v@ucb.edu.bo>
2026-04-27 02:53:17 -0400

    configuracion para deploy

--- app/main.py
+++ app/main.py
@@ -35,18 +35,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
     allow_credentials=True,
-    allow_methods=[
-        "GET",
-        "POST",
-        "PUT",
-        "PATCH",
-        "DELETE",
-        "OPTIONS"
-    ],
-    allow_headers=[
-        "Authorization",
-        "Content-Type"
-    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
